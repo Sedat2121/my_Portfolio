b723bcbd7a3020429682eb5bd8fb6bff803e0ec4
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-12-17 09:46:52 +0000

    email submission part is completed

--- app.js
+++ app.js
@@ -22,3 +22,22 @@ function openmenu() {
 function closemenu() {
     sidemenu.style.right = "-200px";
 }
+
+// email submission
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwV2oKGQQa7ybs2euYrJeTBwnXHBxhs2LmtttPL8Ob73PGHN7T94TG_gnih-7O8Fs--AA/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Your message has been sent successfully!"
+        setTimeout(function() {
+            msg.innerHTML = "";
+        }, 50000);
+        form.reset();
+    })
+    .catch(error => console.error('Error!', error.message))
+});
